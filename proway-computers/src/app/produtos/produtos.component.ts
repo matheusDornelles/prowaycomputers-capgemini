e64bef78da0680706de8972406b1dd2ed8b43b7b
Matheus Dornelles <mdmaia91@gmail.com>
2023-03-27 22:14:57 -0300

    produtos

---
+++ proway-computers/src/app/produtos/produtos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduto, produtos } from '../produtos';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit{
+
+  produtos: IProduto[] = produtos;
+
+  constructor() {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOninit(): void{
+
+  }
+
+}
